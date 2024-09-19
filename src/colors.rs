755985538af05f38feb97388a464a9d240061011
zachcp <zachcp@users.noreply.github.com>
2024-09-19 13:33:39 -0400

    Add Tests; remove unused code (#8)
    * update color tests

--- src/colors.rs
+++ src/colors.rs
@@ -1,16 +1,23 @@
 //! Colors
 //!
-//! This moduel defines the color mapping used for rendering.
+//! This module defines the color mapping used for rendering.
 use bevy::prelude::Color;
 use pdbtbx::Atom;
 
+/// Represents different color schemes for rendering atoms.
 #[derive(Clone)]
 pub enum ColorScheme {
+    /// A solid, sinel color for all atoms.
     Solid(Color),
+    /// Colors atoms based on their element type.
     ByAtomType,
+    // /// Colors atoms based on the chain they belong to.
     // ByChain(Box<dyn Fn(&Chain) -> Color>),
+    // /// Colors atoms based on the secondary structure of their residue.
     // BySecondaryStructure(Box<dyn Fn(&Residue) -> Color>),
+    // /// Colors atoms based on their residue type.
     // ByResidueType(Box<dyn Fn(&Residue) -> Color>),
+    // /// Custom coloring function that takes atom, residue, and chain information.
     // Custom(Box<dyn Fn(&Atom, &Residue, &Chain) -> Color>),
 }
 
@@ -20,7 +27,7 @@ pub enum ColorScheme {
 // ColorScheme::Custom(func) => func(atom, residue, chain),
 impl ColorScheme {
     pub fn get_color(&self, atom: &Atom) -> Color {
-        match self {
+        match &self {
             ColorScheme::Solid(color) => *color,
             ColorScheme::ByAtomType => {
                 match atom.element().expect("expect atom").symbol() {
@@ -32,5 +39,44 @@ impl ColorScheme {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pdbtbx::Atom;
+
+    #[test]
+    fn test_get_color() {
+        let by_atom_scheme = ColorScheme::ByAtomType;
+        let create_atom =
+            |element: &str| Atom::new(true, 1, "", 0.0, 0.0, 0.0, 0.0, 0.0, element, 1).unwrap();
+        let carbon_atom = create_atom("C");
+        let nitrogen_atom = create_atom("N");
+        let oxygen_atom = create_atom("O");
+        let sulfur_atom = create_atom("S");
+        // let other_atom = create_atom("X");
+        // Test ByAtomType color scheme
+        assert_eq!(
+            by_atom_scheme.get_color(&carbon_atom),
+            Color::srgb(0.5, 0.5, 0.5)
+        );
+        assert_eq!(
+            by_atom_scheme.get_color(&nitrogen_atom),
+            Color::srgb(0.0, 0.0, 1.0)
+        );
+        assert_eq!(
+            by_atom_scheme.get_color(&oxygen_atom),
+            Color::srgb(1.0, 0.0, 0.0)
+        );
+        assert_eq!(
+            by_atom_scheme.get_color(&sulfur_atom),
+            Color::srgb(1.0, 1.0, 0.0)
+        );
+        // assert_eq!(
+        //     by_atom_scheme.get_color(&other_atom),
+        //     Color::srgb(1.0, 1.0, 1.0)
+        // );
     }
 }
